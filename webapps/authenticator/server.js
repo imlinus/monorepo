1f22494a9a8b63c934b5b81364d785d7903fa4bc
Linus <liljalinus@gmail.com>
2021-12-02 16:40:38 +0100

    more docker crap

---
+++ webapps/authenticator/server.js
@@ -0,0 +1,15 @@
+import server from '@imlinus/http-server'
+
+import { URL } from 'url'
+import path from 'path'
+
+const __dirname = (() => {
+  let x = path.dirname(decodeURI(new URL(import.meta.url).pathname))
+  return path.resolve((process.platform == 'win32') ? x.substr(1) : x)
+})()
+
+server({
+  src: __dirname, // path.join(__dirname, '/dist'),
+  port: 1338,
+  main: 'index.html'
+})
